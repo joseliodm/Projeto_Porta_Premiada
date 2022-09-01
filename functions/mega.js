d236202fcfdda2379246590d8de9111b5a523a82
Joselio Dias Mendonca <joseliodm@gmail.com>
2022-09-01 20:50:37 -0300

    Atualizacao

--- functions/mega.js
+++ functions/mega.js
@@ -4,7 +4,7 @@ function mega(qtde = 6, numeros = []){
     }
 
     if(numeros.length === qtde){
-        return numeros
+        return numeros.sort((n1, n2) => n1 - n2)
     }
 
     const numeroAleatorio = parseInt(Math.random() * 60) + 1
@@ -16,4 +16,5 @@ function mega(qtde = 6, numeros = []){
     }
 
 }
+
 console.log(mega());

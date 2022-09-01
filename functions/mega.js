302ba3e2daad62a8fad2e94b3233ac31673df018
Joselio Dias Mendonca <joseliodm@gmail.com>
2022-09-01 20:03:41 -0300

    alteracao

---
+++ functions/mega.js
@@ -0,0 +1,19 @@
+function mega(qtde = 6, numeros = []){
+    if(qtde < 6 && qtde > 60){
+        throw "quantidade inválida!"
+    }
+
+    if(numeros.length === qtde){
+        return numeros
+    }
+
+    const numeroAleatorio = parseInt(Math.random() * 60) + 1
+
+    if(!numeros.includes(numeroAleatorio)){
+        return mega(qtde,[...numeros, numeroAleatorio])
+    } else{
+        return mega(qtde, numeros)
+    }
+
+}
+console.log(mega());
